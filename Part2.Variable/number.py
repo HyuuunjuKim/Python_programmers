84cae844512712e5e5e7d783bae31fb5ed181058
HyuuunjuKim <gus7wn@gmail.com>
2019-06-30 16:43:18 +0900

    number

---
+++ Part2.Variable/number.py
@@ -0,0 +1,16 @@
+my_name = "Python" #문자열
+my_age = 23 #숫자
+print(my_name, '은 이제', my_age, '살')
+
+# python은 문자보다는 숫자를 더 유용하게 사용함
+print(my_age+1, '살 되기 싫다')
+
+multiply = 9 * 9 # = 81
+divide = 30 / 3 # = 10
+power = 2 ** 10 # = 1024
+reminder = 15 % 4 # = 3
+print(multiply, divide, power, reminder)
+
+text = '2015' + '1111'
+number = 2015 + 1111
+print(text, number)
