84460001ed265025138c034bb849586826113c25
HyuuunjuKim <gus7wn@gmail.com>
2019-07-14 03:03:15 +0900

    function

---
+++ Part3.If/if.py
@@ -0,0 +1,11 @@
+people = 3
+apple = 20
+
+if people < apple / 5 :
+    print("사과 먹장")
+
+if apple % people > 0 :
+    print("쪼개 먹장")
+
+if people > apple :
+    print("사람 너무 많당 ㅎㅎ..")
---
+++ Part4.Function/function.py
@@ -0,0 +1,13 @@
+def function() :
+    print('안녕, 함수!')
+
+function()
+# 함수는 코드를 모아두고 나중에 쓸 수 있도록 이름을 붙혀둔 것
+
+print()  #내장 함수
+
+def print_round(number) :
+    rounded = round(number)
+    print(rounded)
+
+print_round(4.6)
